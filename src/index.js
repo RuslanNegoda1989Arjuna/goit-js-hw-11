51fd8a875fd96b329e567fd04637769881b206c1
Ruslan Negoda <iarjuna108@gmail.com>
2022-09-21 16:01:41 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Notiflix from 'notiflix';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const axios = require('axios');
+
+// e.g. Only message
+Notiflix.Notify.success('Sol lucet omnibus');
+
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+Notiflix.Notify.warning('Memento te hominem esse');
+
+Notiflix.Notify.info('Cogito ergo sum');
+
+// e.g. Message with a callback

0af27516e60d665ebe8b022b701a36985a57a3aa
Ruslan Negoda <iarjuna108@gmail.com>
2022-09-25 22:17:45 +0300

    1

--- src/index.js
+++ src/index.js
@@ -30,21 +30,31 @@ function onSubmit(evt) {
     evt.currentTarget.elements.searchQuery.value.trim();
   picturesApiSeartch.clearPage();
 
+  clearPicture();
+
   console.log(picturesApiSeartch.queryPic);
 
-  // start fetch
+  // start fetch Ф-ція отримання зображень
   picturesApiSeartch.getPictures().then(data => {
     console.log(data);
+    console.log(data.hits);
+
+    if (data.hits === []) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
     murckupCard(data);
   });
 }
 
 function onLoadeMore() {
   picturesApiSeartch.getPictures().then(data => {
-    murckupCard(data);
+    murckupCardLoadMore(data);
   });
 }
-
+// Ф-ція відмальовування перших картинок
 function murckupCard(data) {
   const murkup = data.hits
     .map(
@@ -75,7 +85,51 @@ function murckupCard(data) {
     .join('');
 
   refs.gallery.insertAdjacentHTML('afterbegin', murkup);
+  lightbox.refresh();
 }
+// Ф-ція відмальовування дозавантаження.
+function murckupCardLoadMore(data) {
+  const murkup = data.hits
+    .map(
+      data => `<div class="photo-card">
+  <a href="${data.largeImageURL}">
+    <img class="photo-img" src="${data.webformatURL}" alt="${data.tags}" loading="lazy" />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${data.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${data.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${data.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${data.downloads}
+    </p>
+  </div>
+  </div>`
+    )
+    .join('');
+
+  refs.gallery.insertAdjacentHTML('beforeend', murkup);
+  lightbox.refresh();
+}
+//  Функція очистки завантежених сторінок
+function clearPicture() {
+  refs.gallery.innerHTML = '';
+}
+//  Lightbox бібліотека підключення
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 // e.g. Only message
 Notiflix.Notify.success('Sol lucet omnibus');
